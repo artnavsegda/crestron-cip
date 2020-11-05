e50114bac6b2a1b448df8c085925b15ac524cae6
Art Navsegda <artnavsegda@gmail.com>
2020-11-05 11:07:46 +0300

    Update cip.js

--- cip.js
+++ cip.js
@@ -9,7 +9,7 @@ var digital = new Array(10000);
 var analog = new Array(10000);
 
 exports.connect = (params, callback) => {
-    //console.log("connecting to " + params.host);
+    console.log("connecting to " + params.host);
 
     client = net.createConnection({ port: 41794, host: params.host}, () => {
         callback();
@@ -20,19 +20,19 @@ exports.connect = (params, callback) => {
     
     client.on('data', (data) => {
         let index = 0;
-        //console.log("data length:" + data.length);
-        //console.log(data.toString('hex'));
+        console.log("data length:" + data.length);
+        console.log(data.toString('hex'));
     
         while (index < data.length)
         {
             let payloadType = data[index];
-            //console.log("type: 0x" + payloadType.toString(16));
+            console.log("type: 0x" + payloadType.toString(16));
     
             let payloadLength = data[index + 2]
-            //console.log("payloadLength: " + payloadLength);
+            console.log("payloadLength: " + payloadLength);
     
             let payload = data.slice(index+3, index+3+payloadLength);
-            //console.log("payloadData: " + payload.toString('hex'));
+            console.log("payloadData: " + payload.toString('hex'));
     
             switch (payloadType)
             {
@@ -45,27 +45,27 @@ exports.connect = (params, callback) => {
                     client.write("\x05\x00\x05\x00\x00\x02\x03\x00");
                 break;
                 case 0x05:
-                    //console.log("data");
+                    console.log("data");
                     switch(payload[3])
                     {
                         case 0x0:
-                            //console.log("digital join " + ((((payload[5] & 0x7F) << 8) | payload[4]) + 1) + " state " + (((payload[5] & 0x80) >> 7) ^ 0x01));
+                            console.log("digital join " + ((((payload[5] & 0x7F) << 8) | payload[4]) + 1) + " state " + (((payload[5] & 0x80) >> 7) ^ 0x01));
                             digital[((((payload[5] & 0x7F) << 8) | payload[4]) + 1)] = (((payload[5] & 0x80) >> 7) ^ 0x01);
                             cipEvents.emit("data", {type: "digital", join: (((payload[5] & 0x7F) << 8) | payload[4]) + 1, value: (((payload[5] & 0x80) >> 7) ^ 0x01)});
                         break;
                         case 0x14:
-                            //console.log("analog join " + (((payload[4] << 8) | payload[5]) + 1) + " value " + ((payload[6] << 8) + payload[7]));
+                            console.log("analog join " + (((payload[4] << 8) | payload[5]) + 1) + " value " + ((payload[6] << 8) + payload[7]));
                             analog[(((payload[4] << 8) | payload[5]) + 1)] = ((payload[6] << 8) + payload[7]);
                             cipEvents.emit("data", {type: "analog", join: ((payload[4] << 8) | payload[5]) + 1, value: (payload[6] << 8) + payload[7]});
                         break;
                         case 0x03:
-                            //console.log("update request");
+                            console.log("update request");
                         break;
                     }
                 break;
                 case 0x0D:
                 case 0x0E:
-                    //console.log("heartbeat");
+                    console.log("heartbeat");
                 break;
             }
             index = index + payloadLength + 3;
